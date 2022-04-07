97d9e0e50e14c2beade2bd587fce0eadadccc06a
Daniel Pilligua <daniel.pilligua.7e4@itb.cat>
2022-04-07 15:22:22 +0200

    a

--- practicaM15.js
+++ practicaM15.js
@@ -62,7 +62,7 @@ function draw() { // this function code runs in infinite loop
 
         // Apply pepe and cigar
         image(pepe, singlePose.nose.x-50, singlePose.nose.y-50, 125, 125);
-        image(monalisa, singlePose.nose.x, singlePose.nose.y, 50, 50);
+        image(monalisa, singlePose.nose.x-100, singlePose.nose.y-100, 50, 50);
     }
     
 }
